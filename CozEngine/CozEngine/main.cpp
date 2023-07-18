66dacada9510b7f760056a4a2853b09fd661f9ad
Corrie Anderson <C0zz4a@hotmail.co.uk>
2023-07-18 12:17:01 +0100

    Window
    - Setup initial window
    - Created engine folder

--- CozEngine/CozEngine/main.cpp
+++ CozEngine/CozEngine/main.cpp
@@ -1,7 +1,64 @@
+#include <iostream>
+#include <memory>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Temp input function. Should eventually setup input system.
+void ProcessInput(GLFWwindow* Window)
+{
+	if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(Window, true);
+	}
+}
+
 int main()
 {
+	glfwInit();
+
+	// Window config info: http://www.glfw.org/docs/latest/window.html#window_hints
+
+	// >= Open GL 3.3 required
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* Window = glfwCreateWindow(800, 600, "CozEngine - GLFW Window", NULL, NULL);
+	if (!Window)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(Window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(Window, [](GLFWwindow* Window, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		});
+
+	while (!glfwWindowShouldClose(Window))
+	{
+		ProcessInput(Window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(Window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
