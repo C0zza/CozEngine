d28f6f4a0fbfe396617d0e92985c73a59141428b
Corrie Anderson <C0zz4a@hotmail.co.uk>
2023-07-18 14:13:58 +0100

    Drawing a triangle
    Very messy code.

--- CozEngine/CozEngine/main.cpp
+++ CozEngine/CozEngine/main.cpp
@@ -4,12 +4,50 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Temp shaders
+const char* VertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+const char* FragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
 // Temp input function. Should eventually setup input system.
 void ProcessInput(GLFWwindow* Window)
 {
 	if (glfwGetKey(Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(Window, true);
+	}
+}
+
+void CheckShaderCompilation(unsigned int ID)
+{
+	int Success;
+	char InfoLog[512];
+	glGetShaderiv(ID, GL_COMPILE_STATUS, &Success);
+	if (!Success)
+	{
+		glGetShaderInfoLog(ID, 512, NULL, InfoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << InfoLog << "\n";
+	}
+}
+
+void CheckProgramCompilation(unsigned int ID)
+{
+	int Success;
+	char InfoLog[512];
+	glGetProgramiv(ID, GL_COMPILE_STATUS, &Success);
+	if (!Success)
+	{
+		glGetProgramInfoLog(ID, 512, NULL, InfoLog);
+		std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n" << InfoLog << "\n";
 	}
 }
 
@@ -47,12 +85,53 @@ int main()
 			glViewport(0, 0, width, height);
 		});
 
+	float Vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Any buffer calls for GL_ARRAY_BUFFER now affect VBO.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+	unsigned int VertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShader, 1, &VertexShaderSource, NULL);
+	glCompileShader(VertexShader);
+	CheckShaderCompilation(VertexShader);
+
+	unsigned int FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragmentShader, 1, &FragmentShaderSource, NULL);
+	glCompileShader(FragmentShader);
+	CheckShaderCompilation(FragmentShader);
+
+	unsigned int ShaderProgram;
+	ShaderProgram = glCreateProgram();
+	glAttachShader(ShaderProgram, VertexShader);
+	glAttachShader(ShaderProgram, FragmentShader);
+	glLinkProgram(ShaderProgram);
+
+	glDeleteShader(VertexShader);
+	glDeleteShader(FragmentShader);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glUseProgram(ShaderProgram);
+
 	while (!glfwWindowShouldClose(Window))
 	{
 		ProcessInput(Window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(Window);
 		glfwPollEvents();
