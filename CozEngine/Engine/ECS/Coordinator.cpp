7f4b6cd34c64b92aa47ece917c9bceab4e336d75
Corrie Anderson <C0zz4a@hotmail.co.uk>
2023-10-06 23:58:12 +0100

    Forgot a few definitions

--- CozEngine/Engine/ECS/Coordinator.cpp
+++ CozEngine/Engine/ECS/Coordinator.cpp
@@ -1,1 +1,20 @@
 #include "Coordinator.h"
+
+void LCoordinator::Init()
+{
+	ComponentManager = std::make_unique<LComponentManager>();
+	EntityManager = std::make_unique<LEntityManager>();
+	SystemManager = std::make_unique<LSystemManager>();
+}
+
+LEntity LCoordinator::CreateEntity()
+{
+	return EntityManager->CreateEntity();
+}
+
+void LCoordinator::DestroyEntity(LEntity Entity)
+{
+	EntityManager->DestroyEntity(Entity);
+	ComponentManager->EntityDestroyed(Entity);
+	SystemManager->EntityDestroyed(Entity);
+}
