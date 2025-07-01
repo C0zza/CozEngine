24792af0ae5f4870f3d80477e1111715b9814913
Corrie Anderson <C0zz4a@hotmail.co.uk>
2025-07-01 20:52:51 +0100

    Reflect LECSComponent

--- CozEngine/Engine/ECS/ECSComponents/ECSComponent.h
+++ CozEngine/Engine/ECS/ECSComponents/ECSComponent.h
@@ -3,11 +3,13 @@
 #include "ECS/ECSDefinitions.h"
 #include "json.hpp"
 #include "Misc/FuncRegister.h"
+#include "Reflection/Reflection.h"
 
 using LComponentRegister = LFuncRegister<nlohmann::json>;
 	
 class LECS;
 
+REFL_CLASS(Abstract)
 struct LECSComponent
 {
 	LEntityID EntityID = 0;
@@ -20,5 +22,7 @@ protected:
 	virtual void Init() {}
 
 	LECS* ECS = nullptr;
+
+	REFL_GENERATED_BODY(LECSComponent)
 };
 
---
+++ CozEngine/Engine/GeneratedFiles/ECSComponentGen.cpp
@@ -0,0 +1,74 @@
+#include "ECS/ECSComponents/ECSComponent.h"
+
+#include "Editor/ImGuiPropertyDrawHelpers.h"
+#include "Reflection/Class.h"
+#include "Reflection/ClassRegister.h"
+#include "Reflection/ClassUtilities.h"
+#include "Reflection/Property.h"
+
+
+LClass* LECSComponent::Class = LECSComponent::StaticClass();
+
+LClass* LECSComponent::StaticClass()
+{
+    if(!LECSComponent::Class)
+    {
+        LECSComponent::Class = new LClass();
+        assert(LECSComponent::Class);
+
+        std::vector<LProperty> Properties
+        {
+
+        };
+
+        std::function<void(uint8_t*)> DrawEditorFunc = [](uint8_t* Address)
+            {
+
+            };
+
+        
+        std::function<void*()> CreateObjectFunc = []()
+            {
+                return nullptr;
+            };
+
+        std::function<void(const uint8_t*, nlohmann::json& Json)> SerializeFunc = [](const uint8_t* Address, nlohmann::json& Json)
+            {
+                const LECSComponent* Object = reinterpret_cast<const LECSComponent*>(Address);
+                to_json(Json, *Object);
+            };
+
+        std::function<void(uint8_t*, const nlohmann::json& Json)> DeserializeFunc = [](uint8_t* Address, const nlohmann::json& Json)
+            {
+                LECSComponent* Object = reinterpret_cast<LECSComponent*>(Address);
+                from_json(Json, *Object);
+            };
+
+        LClassUtilities::RegisterStaticClassProperties(LECSComponent::Class,
+                                                        Properties,
+                                                        sizeof(LECSComponent),
+                                                        alignof(LECSComponent),
+                                                        "LECSComponent",
+                                                        "",
+                                                        DrawEditorFunc,
+                                                        CreateObjectFunc,
+                                                        SerializeFunc,
+                                                        DeserializeFunc);
+        
+        LClassRegister::RegisterObj("LECSComponent", LECSComponent::Class);
+    }
+    return LECSComponent::Class;
+}
+
+void from_json(const nlohmann::json& Json, LECSComponent& Object)
+{
+
+
+}
+
+void to_json(nlohmann::json& Json, const LECSComponent& Object)
+{
+
+    Json["Type"] = "LECSComponent";
+
+}
