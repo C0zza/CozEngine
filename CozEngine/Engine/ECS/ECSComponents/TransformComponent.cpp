f3c4063d56e465ce54979670d50deb0ef6a82b92
Corrie Anderson <C0zz4a@hotmail.co.uk>
2025-07-05 19:21:26 +0100

    Forgot to include moved transform logic on component

--- CozEngine/Engine/ECS/ECSComponents/TransformComponent.cpp
+++ CozEngine/Engine/ECS/ECSComponents/TransformComponent.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 #include "TransformComponent.h"
 
+#include <glm/common.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 #include "Misc/GlmSerialization.h"
 
 #if defined(COZ_EDITOR)
@@ -24,4 +27,109 @@ void CTransformComponentSystem::DeserializeComponent(CTransformComponent& Compon
 	Component.SetPosition(J["Position"]);
 	Component.SetRotation(J["Rotation"]);
 	Component.SetScale(J["Scale"]);
+}
+
+void Clamp(float& Value, const float Min, const float Max)
+{
+	if (Value > Max)
+	{
+		Value -= Max;
+	}
+	else if (Value < Min)
+	{
+		Value += Max;
+	}
+}
+
+CTransformComponent::CTransformComponent()
+	: Position{ glm::vec3(0.f, 0.f, 0.f) }, Rotation{ glm::vec3(0.f, 0.f, 0.f) }, Scale{ glm::vec3(1.f, 1.f, 1.f) },
+	TransformationMatrix{ glm::mat4(1.f) }
+{
+}
+
+void CTransformComponent::MoveRelative(const glm::vec3& Movement)
+{
+	glm::vec3 RotatedMovement = Movement;
+	RotatedMovement = glm::rotateX(RotatedMovement, Rotation.x);
+	RotatedMovement = glm::rotateY(RotatedMovement, Rotation.y);
+	RotatedMovement = glm::rotateZ(RotatedMovement, Rotation.z);
+
+	Position.x += RotatedMovement.x;
+	Position.y += RotatedMovement.y;
+	Position.z += RotatedMovement.z;
+	IsDirty = true;
+}
+
+void CTransformComponent::Move(const glm::vec3& Movement)
+{
+	Position.x += Movement.x;
+	Position.y += Movement.y;
+	Position.z += Movement.z;
+	IsDirty = true;
+}
+
+void CTransformComponent::Rotate(const glm::vec3& Rotate)
+{
+	Rotation += Rotate;
+
+	float Min = 0.f, Max = 360.f;
+	/*Clamp(Rotation.x, Min, Max);
+	Clamp(Rotation.y, Min, Max);
+	Clamp(Rotation.z, Min, Max);*/
+
+	IsDirty = true;
+}
+
+glm::vec3 CTransformComponent::GetForward() const
+{
+	glm::vec3 Result = glm::vec3(0.f, 0.f, -1.f);
+	Result = glm::rotateX(Result, Rotation.x);
+	Result = glm::rotateY(Result, Rotation.y);
+	Result = glm::rotateZ(Result, Rotation.z);
+	return glm::normalize(Result);
+}
+
+glm::vec3 CTransformComponent::GetRight() const
+{
+	glm::vec3 Result = glm::vec3(1.f, 0.f, 0.f);
+	Result = glm::rotateX(Result, Rotation.x);
+	Result = glm::rotateY(Result, Rotation.y);
+	Result = glm::rotateZ(Result, Rotation.z);
+	return glm::normalize(Result);
+}
+
+glm::vec3 CTransformComponent::GetUp() const
+{
+	glm::vec3 Result = glm::vec3(0.f, 1.f, 0.f);
+	Result = glm::rotateX(Result, Rotation.x);
+	Result = glm::rotateY(Result, Rotation.y);
+	Result = glm::rotateZ(Result, Rotation.z);
+	return glm::normalize(Result);
+}
+
+void CTransformComponent::UpdateTransformationMatrix()
+{
+	TransformationMatrix = glm::mat4(1.f);
+	TransformationMatrix = glm::translate(TransformationMatrix, Position);
+	TransformationMatrix = glm::rotate(TransformationMatrix, Rotation.x, glm::vec3(1.0, 0.f, 0.f));
+	TransformationMatrix = glm::rotate(TransformationMatrix, Rotation.y, glm::vec3(0.f, 1.0f, 0.f));
+	TransformationMatrix = glm::rotate(TransformationMatrix, Rotation.z, glm::vec3(0.f, 0.f, 1.0f));
+	TransformationMatrix = glm::scale(TransformationMatrix, Scale);
+
+	IsDirty = false;
+}
+
+const glm::mat4& CTransformComponent::GetTransformationMatrix() const
+{
+	return TransformationMatrix;
+}
+
+const glm::mat4& CTransformComponent::GetUpdatedTransformationMatrix()
+{
+	if (IsDirty)
+	{
+		UpdateTransformationMatrix();
+	}
+
+	return GetTransformationMatrix();
 }
--- CozEngine/Engine/ECS/ECSComponents/TransformComponent.h
+++ CozEngine/Engine/ECS/ECSComponents/TransformComponent.h
@@ -2,11 +2,49 @@
 
 #include "ECS/ComponentSystem.h"
 #include "ECS/ECSComponents/ECSComponent.h"
-#include "Transform.h"
 
 // Temporarily inherit LTransform to avoid rewriting it all here
-struct CTransformComponent : public LTransform, LECSComponent
+REFL_CLASS()
+struct CTransformComponent : public LECSComponent
 {
+	REFL_GENERATED_BODY(CTransformComponent)
+public:
+	CTransformComponent();
+
+	REFL_PROP(Visible)
+	glm::vec3 Position;
+	REFL_PROP(Visible)
+	glm::vec3 Rotation;
+	REFL_PROP(Visible)
+	glm::vec3 Scale;
+
+public:
+	const glm::vec3& GetPosition() const { return Position; }
+	const glm::vec3& GetRotation() const { return Rotation; }
+	const glm::vec3& GetScale() const { return Scale; }
+
+	void SetPosition(const glm::vec3& NewPosition) { Position = NewPosition; IsDirty = true; }
+	void SetRotation(const glm::vec3& NewRotation) { Rotation = NewRotation; IsDirty = true; }
+	void SetScale(const glm::vec3& NewScale) { Scale = NewScale; IsDirty = true; }
+
+	void Move(const glm::vec3& Movement);
+	void MoveRelative(const glm::vec3& Movement);
+	void Rotate(const glm::vec3& Rotate);
+	void ScaleByFloat(const float Scalar) { Scale *= Scalar; IsDirty = true; }
+
+	glm::vec3 GetForward() const;
+	glm::vec3 GetRight() const;
+	glm::vec3 GetUp() const;
+
+	void UpdateTransformationMatrix();
+	const glm::mat4& GetTransformationMatrix() const;
+	const glm::mat4& GetUpdatedTransformationMatrix();
+
+	bool GetIsDirty() const { return IsDirty; }
+
+private:
+	glm::mat4 TransformationMatrix;
+	bool IsDirty = true;
 };
 
 class CTransformComponentSystem : public LComponentSystem<CTransformComponent>
